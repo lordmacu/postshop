225b60453591af0960f9d724f50ff53aaf05482e
Cristian garcia <cristian.garcia@pegasi.io>
2021-09-21 15:37:49 -0500

    add product detail edit

--- lib/products/controllers/ProductContoller.dart
+++ lib/products/controllers/ProductContoller.dart
@@ -1,8 +1,15 @@
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProductContoller extends GetxController{
 
   var indificualTicket=false.obs;
   var categorySelect=0.obs;
+  var selectedColor = "0xff298dcf".obs;
+  var isFormSelected = false.obs;
+  var selectedForm = "square".obs;
+  var step = 0.obs;
+
 }
--- lib/products/products.dart
+++ lib/products/products.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:poshop/products/controllers/ProductContoller.dart';
+import 'package:poshop/products/detail.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:get/get.dart';
 
 class ProductsList extends StatelessWidget {
+  ProductContoller controllerHome = Get.put(ProductContoller());
+
   @override
   Widget build(BuildContext context) {
     PanelController _panelController = PanelController();
@@ -13,183 +18,7 @@ class ProductsList extends StatelessWidget {
         controller: _panelController,
         minHeight: 0,
 
-        panel: Container(
-          padding: EdgeInsets.only(top: 20, left: 20, right: 20),
-          child: Column(
-            children: [
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "#15312",
-                          style: TextStyle(fontSize: 25, color: Colors.grey),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 5),
-                          child: Text(
-                            "08/09/2021 8:30am",
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        RaisedButton(
-                          onPressed: () {},
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          color: Color(0xff298dcf),
-                          child: Text(
-                            "Reembolsar",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(left: 5),
-                          width: 60,
-                          child: RaisedButton(
-
-                            onPressed: () {},
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                            ),
-                            color: Colors.white,
-                            child: Icon(Icons.email,color:  Color(0xff298dcf),),
-                           ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10, top: 10),
-                height: 1,
-                child: null,
-                color: Colors.grey.withOpacity(0.3),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: Column(
-                  children: [
-                    Text(
-                      "\$10.000",
-                      style: TextStyle(fontSize: 40),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 5),
-                      padding: EdgeInsets.only(
-                          left: 10, right: 10, top: 5, bottom: 5),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Color(0xff298dcf).withOpacity(0.7),
-                      ),
-                      child: Text(
-                        "Total",
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: Row(
-                  children: [
-                    Text(
-                      "Cajero:",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 5),
-                      child: Text(
-                        "Propietario",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              
-              Expanded(
-                  child: Container(
-                child: ListView.builder(
-                    itemCount: 50,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        padding: EdgeInsets.only(top: 20,bottom: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              children: [
-                                Container(
-                                  child: Text("Primer Articulo"),
-                                ),
-                                Container(
-                                  child: Text("1 X 10.000,00"),
-                                )
-                              ],
-                            ),
-                            Container(
-                              child: Text("10.000,00"),
-                            )
-                          ],
-                        ),
-                      );
-                    }),
-              )),
-             Column(
-               children: [
-                 Container(
-                   margin: EdgeInsets.only(bottom: 10, top: 10),
-                   height: 1,
-                   child: null,
-                   color:  Color(0xff298dcf),
-                 ),
-                 Container(
-                   padding: EdgeInsets.only(bottom: 10),
-                   child: Row(
-                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                     children: [
-                       Column(
-                         mainAxisAlignment: MainAxisAlignment.start,
-                         crossAxisAlignment: CrossAxisAlignment.start,
-                         children: [
-                           Text("Total",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                           Container(
-                             margin: EdgeInsets.only(top: 5),
-                             child: Text("Efectivo",style: TextStyle(fontSize: 18),),
-                           )
-                         ],
-                       ),
-                       Column(
-                         mainAxisAlignment: MainAxisAlignment.end,
-                         crossAxisAlignment: CrossAxisAlignment.end,
-                         children: [
-                           Text("10.000,00",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                           Container(
-                             margin: EdgeInsets.only(top: 5),
-                             child: Text("10.000,00",style: TextStyle(fontSize: 18),),
-                           )
-                         ],
-                       )
-                     ],
-                   ),
-                 )
-               ],
-             )
-            ],
-          ),
-        ),
+        panel: DetailProduct(),
         body: Column(
           children: [
             Padding(
